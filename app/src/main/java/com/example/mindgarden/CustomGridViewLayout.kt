5617bdc8c77e30bc2e8056b839ae9ea29b86416e
chungee375@gmail.com <chungee375@gmail.com>
2019-07-06 15:02:23 +0900

    commit
    환경설정/스플래시/로그인

---
+++ app/src/main/java/com/example/mindgarden/CustomGridViewLayout.kt
@@ -0,0 +1,3 @@
+package com.example.mindgarden
+
+class CustomGridViewLayout (var Image: Int)
---
+++ app/src/main/java/com/example/mindgarden/SharedPreferenceController.kt
@@ -0,0 +1,40 @@
+package com.example.mindgarden
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object SharedPreferenceController {
+    val MY_ACCOUNT="unique_string"
+    val PASSWORD="unique_string2"
+
+    fun setUserID(ctx: Context,userId:String){
+        val  preference: SharedPreferences=ctx.getSharedPreferences(MY_ACCOUNT,Context.MODE_PRIVATE)
+        val  editor:SharedPreferences.Editor=preference.edit()
+        editor.putString("u_id",userId)
+        editor.commit()
+    }
+    fun getUserID(ctx:Context):String{
+        val preference:SharedPreferences=ctx.getSharedPreferences(MY_ACCOUNT,Context.MODE_PRIVATE)
+        return preference.getString("u_id","")
+    }
+    fun clearUserID(ctx:Context){
+        val preference:SharedPreferences=ctx.getSharedPreferences(MY_ACCOUNT,Context.MODE_PRIVATE)
+        val editor:SharedPreferences.Editor=preference.edit()
+        //로그아웃하면 앱 내 비밀번호도 없어진다!
+        editor.clear()
+        editor.commit()
+    }
+    fun setPassword(ctx: Context,pw:String){
+        val  preference: SharedPreferences=ctx.getSharedPreferences(PASSWORD,Context.MODE_PRIVATE)
+        val  editor:SharedPreferences.Editor=preference.edit()
+        editor.putString("PW",pw)
+        editor.apply()
+    }
+    fun getPassword(ctx:Context):String{
+        val preference:SharedPreferences=ctx.getSharedPreferences(PASSWORD,Context.MODE_PRIVATE)
+        return preference.getString("PW","")
+    }
+
+
+
+}
