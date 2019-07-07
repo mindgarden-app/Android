cda07e69ac10a1b9308d38ead6887f42fd1c7ba2
chungee375@gmail.com <chungee375@gmail.com>
2019-07-08 05:49:24 +0900

    <수정>manifest XML 카메라 권한 수정( 다중권한 되는지 확인하기 위해서) <추가>다중 권한 받기

--- app/src/main/java/com/example/mindgarden/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/mindgarden/Activity/LoginActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mindgarden.Activity
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.support.design.widget.TabLayout
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
@@ -13,6 +17,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import com.example.mindgarden.R
 import com.example.mindgarden.Adapter.SliderLoginPagerAdapter
 import com.example.mindgarden.DB.SharedPreferenceController
@@ -21,13 +26,22 @@ import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 class LoginActivity : AppCompatActivity() {
-    private val RECORD_REQUEST_CODE = 101
-    val REQUEST_CODE_LOGIN_ACTIVITY=100
+    private val PERMISSION_CALLBACK_CONSTANT = 101
+    private val REQUEST_PERMISSION_SETTING = 101
+    private var permissionsRequired = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private var permissionStatus: SharedPreferences? = null
+    private var sentToSettings = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         configureMainTab()
-        setupPermissions()
+
+        permissionStatus = getSharedPreferences("permissionStatus", Context.MODE_PRIVATE)
+        requestPermission()
+        //setupPermissions(permissionsRequired[0])
+        //setupPermissions(permissionsRequired[1])
+        //setupPermissions(permissionsRequired[2])
 
         btnLogin.setOnClickListener {
 
@@ -42,45 +56,114 @@ class LoginActivity : AppCompatActivity() {
             //  startActivity<PasswordActivity>("from" to  "login")
         }
     }
-    private fun setupPermissions() {
+    private fun requestPermission() {
+        if (ActivityCompat.checkSelfPermission(this, permissionsRequired[0]) != PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this, permissionsRequired[1]) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissionsRequired[0])
+                || ActivityCompat.shouldShowRequestPermissionRationale(this, permissionsRequired[1])) {
+                //Show Information about why you need the permission
+                getAlertDialog()
+                Log.e("per1",permissionsRequired[0])
+            } else if (permissionStatus!!.getBoolean(permissionsRequired[0], false)) {
+                Log.e("per2",permissionsRequired[1])
+                //Previously Permission Request was cancelled with 'Dont Ask Again',
+                // Redirect to Settings after showing Information about why you need the permission
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Need Multiple Permissions")
+                builder.setMessage("This app needs permissions.")
+                builder.setPositiveButton("Grant") { dialog, which ->
+                    dialog.cancel()
+                    sentToSettings = true
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivityForResult(intent, REQUEST_PERMISSION_SETTING)
+                    Toast.makeText(applicationContext, "Go to Permissions to Grant ", Toast.LENGTH_LONG).show()
+                }
+                builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+                builder.show()
+            }  else {
+                Log.e("per3",permissionsRequired[2])
+                //just request the permission
+                ActivityCompat.requestPermissions(this, permissionsRequired, PERMISSION_CALLBACK_CONSTANT)
+            }
+            val editor = permissionStatus!!.edit()
+            editor.putBoolean(permissionsRequired[0], true)
+            editor.commit()
+
+            //   txtPermissions.setText("Permissions Required")
+
+
+        } else {
+            //You already have the permission, just go ahead.
+            Toast.makeText(applicationContext, "Allowed All Permissions", Toast.LENGTH_LONG).show()
+        }
+    }
+    /*private fun setupPermissions(requestPermission: String) {
         //스토리지 읽기 퍼미션을 permission 변수에 담는다
-        val permission = ContextCompat.checkSelfPermission(this,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+        val permission = ContextCompat.checkSelfPermission(this,requestPermission)
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
             Log.e("TAG", "Permission to record denied")
-            makeRequest()
-        }
-    }
-    private fun makeRequest() {
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-            RECORD_REQUEST_CODE)
-    }
+    makeRequest(requestPermission)
+}
+
+}
 
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+private fun makeRequest(requestPermission: String) {
+    ActivityCompat.requestPermissions(this,
+        arrayOf( requestPermission),
+        RECORD_REQUEST_CODE)
+}
 
-        when(requestCode){
-            RECORD_REQUEST_CODE ->{
-//                if(grantResults.isNotEmpty()
-//                            && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    toast("권한거부됨");
-
-                }else{
-                 toast("권한 자 ㄹ있어")
-                    Log.i("tag", "Permission has been granted by user")
+*/
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_CALLBACK_CONSTANT) {
+            //check if all permissions are granted
+            var allgranted = false
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    allgranted = true
+                } else {
+                    allgranted = false
+                    break
                 }
-                return
+            }
+
+            if (allgranted) {
+                Toast.makeText(applicationContext, "Allowed All Permissions", Toast.LENGTH_LONG).show()
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissionsRequired[0])
+                || ActivityCompat.shouldShowRequestPermissionRationale(this, permissionsRequired[1])
+            ) {
+
+                getAlertDialog()
+            } else {
+                Toast.makeText(applicationContext, "Unable to get Permission", Toast.LENGTH_LONG).show()
             }
         }
     }
+        private fun getAlertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Need Multiple Permissions")
+        builder.setMessage("This app needs permissions.")
+        builder.setPositiveButton("Grant") { dialog, i->
+            dialog.cancel()
+            ActivityCompat.requestPermissions(this, permissionsRequired, PERMISSION_CALLBACK_CONSTANT)
+        }
+        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+        builder.show()
+    }
 
-
-    fun postLoginResponse(u_id:String,u_pw:String){
-        SharedPreferenceController.setUserID(this,u_id)
+    override fun onPostResume() {
+        super.onPostResume()
+        if (sentToSettings) {
+            if (ActivityCompat.checkSelfPermission(this, permissionsRequired[0]) == PackageManager.PERMISSION_GRANTED) {
+                //Got Permission
+                Toast.makeText(applicationContext, "Allowed All Permissions", Toast.LENGTH_LONG).show()
+            }
+        }
     }
     private fun configureMainTab() {
 
