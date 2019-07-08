9c988332c415f83e4b71225c0d04eb847cf09c40
Cho2018 <hjchojane@naver.com>
2019-07-08 16:20:02 +0900

    [수정]

--- app/src/main/java/com/example/mindgarden/Adapter/MainPagerAdapter.kt
+++ app/src/main/java/com/example/mindgarden/Adapter/MainPagerAdapter.kt
@@ -3,6 +3,7 @@ package com.example.mindgarden.Adapter
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import com.example.mindgarden.Fragment.BlankFragment
 import com.example.mindgarden.Fragment.DiaryListFragment
 import com.example.mindgarden.Fragment.MainFragment
 import com.example.mindgarden.R
@@ -11,7 +12,7 @@ class MainPagerAdapter(fm: FragmentManager, private val num_fragment: Int): Frag
     override fun getItem(p0: Int): Fragment? {
         return when(p0){
             0 -> MainFragment()
-            1 -> DiaryListFragment()
+            1 -> BlankFragment()
             2 -> DiaryListFragment()
             else -> null
         }
