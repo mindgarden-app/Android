ad249d25ea3ed674bb3eb4b90a0ad76ac7dd4ec1
chungee375@gmail.com <chungee375@gmail.com>
2019-08-18 03:56:26 +0900

    <수정중> 계정삭제- 자동로그인 무한 루프 : 웹뷰의 쿠키 지워야 함

--- app/src/main/java/com/example/mindgarden/Activity/MypageActivity.kt
+++ app/src/main/java/com/example/mindgarden/Activity/MypageActivity.kt
@@ -43,17 +43,20 @@ class MypageActivity : AppCompatActivity() {
         btnLogout.setOnClickListener {
             TokenController.clearRefreshToken(this)
             //TODO 로그아웃 코드 추가해야함
-
             val intent = Intent(this, LoginActivity::class.java)
             //로그아웃 누르면 다시 일단 로그인 페이지로
-
             startActivity(intent)
-
             finish()
         }
         btnDelete.setOnClickListener{
             deleteUserResponse()
             TokenController.clearRefreshToken(this)
+
+            val Intent= Intent(this, WebviewLoginActivity::class.java)
+            Intent.putExtra("whyOpen","deleteUser")
+            startActivity(Intent)
+
+
         }
 
         //LinerLayout 버튼들
