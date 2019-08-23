f59d99f9a3e12c0a419abb0210ce52ea87078921
chungee375@gmail.com <chungee375@gmail.com>
2019-08-23 20:09:23 +0900

    <수정> 계정삭제 다이얼로그 추가

--- app/src/main/java/com/example/mindgarden/Activity/MypageActivity.kt
+++ app/src/main/java/com/example/mindgarden/Activity/MypageActivity.kt
@@ -1,9 +1,12 @@
 package com.example.mindgarden.Activity
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Gravity
 import android.widget.TextView
 import com.example.mindgarden.DB.SharedPreferenceController
 import com.example.mindgarden.DB.TokenController
@@ -41,67 +44,89 @@ class MypageActivity : AppCompatActivity() {
 
         //RelativeLayout
         btnLogout.setOnClickListener {
-            TokenController.clearRefreshToken(this)
-            //TODO 로그아웃 코드 추가해야함
+            //TODO TokenController.clearRefreshToken(this)
+            //TODO 로그아웃 코드 추가해야함 웹쿠키
             val intent = Intent(this, LoginActivity::class.java)
             //로그아웃 누르면 다시 일단 로그인 페이지로
             startActivity(intent)
             finish()
         }
         btnDelete.setOnClickListener{
+            var dlg = AlertDialog.Builder(this, R.style.MyAlertDialogStyle)
+
+            dlg.setMessage("계정 삭제는 이메일로 문의해주세요.\n"+"mindgarden2019@gmail.com")
+
+
+            dlg.setNeutralButton("확인 ", null)
+
+
+            var dlgNew: AlertDialog = dlg.show()
+            var messageText:TextView? = dlgNew.findViewById(android.R.id.message)
+            messageText!!.gravity = Gravity.CENTER
+
+
+            dlgNew.window.setBackgroundDrawableResource(R.drawable.round_layout_border)
+
+            dlgNew.show()
+
+            //TODO 웹쿠키 지우고 내부 디비 지우고 계정삭제
+
+            /*
             deleteUserResponse()
             TokenController.clearRefreshToken(this)
+
+
 
             val Intent= Intent(this, WebviewLoginActivity::class.java)
             Intent.putExtra("whyOpen","deleteUser")
             startActivity(Intent)
+            */
 
+}
 
-        }
+//LinerLayout 버튼들
+btnPasswordSetting.setOnClickListener {
+val intent = Intent(this, PasswordSettingActivity::class.java)
+//암호 설정버튼 누르면 암호 액티비티로 넘어가는 것으로 구현
 
-        //LinerLayout 버튼들
-        btnPasswordSetting.setOnClickListener {
-            val intent = Intent(this, PasswordSettingActivity::class.java)
-            //암호 설정버튼 누르면 암호 액티비티로 넘어가는 것으로 구현
+startActivity(intent)
 
-            startActivity(intent)
+finish()
+}
 
-            finish()
-        }
+alarmSetting.setOnClickListener {
+val intent3 = Intent(this, AlarmSettingActivity::class.java)
+// 알람 설정하는 페이즈로 넘어감
 
-        alarmSetting.setOnClickListener {
-            val intent3 = Intent(this, AlarmSettingActivity::class.java)
-            // 알람 설정하는 페이즈로 넘어감
+startActivity(intent3)
 
-            startActivity(intent3)
+finish()
+}
+}
+private fun deleteUserResponse() {
+if(!TokenController.isValidToken(ctx)){
+RenewAcessTokenController.postRenewAccessToken(ctx)
+}
 
-            finish()
+val deleteDiaryListResponse = networkService.deleteUserResponse(
+TokenController.getAccessToken(ctx))
+Log.e("delete", "delete")
+
+deleteDiaryListResponse.enqueue(object: Callback<DeleteUserResponse> {
+override fun onFailure(call: Call<DeleteUserResponse>, t: Throwable) {
+
+}
+override fun onResponse(call: Call<DeleteUserResponse>, response: Response<DeleteUserResponse>) {
+
+    if (response.isSuccessful) {
+        if (response.body()!!.status == 200) {
+           Log.e("Delete User",response.body()!!.message)
+
         }
     }
-    private fun deleteUserResponse() {
-        if(!TokenController.isValidToken(ctx)){
-            RenewAcessTokenController.postRenewAccessToken(ctx)
-        }
-
-        val deleteDiaryListResponse = networkService.deleteUserResponse(
-            TokenController.getAccessToken(ctx))
-        Log.e("delete", "delete")
-
-        deleteDiaryListResponse.enqueue(object: Callback<DeleteUserResponse> {
-            override fun onFailure(call: Call<DeleteUserResponse>, t: Throwable) {
-
-            }
-            override fun onResponse(call: Call<DeleteUserResponse>, response: Response<DeleteUserResponse>) {
-
-                if (response.isSuccessful) {
-                    if (response.body()!!.status == 200) {
-                       Log.e("Delete User",response.body()!!.message)
-
-                    }
-                }
-            }
+}
 
 
-        })
-    }
+})
+}
 }
