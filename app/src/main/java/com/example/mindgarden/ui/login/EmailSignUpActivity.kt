3c648d371d6dd056edbbb9db6390752bb2c1e435
EuneeChung <chungee375@gmail.com>
2020-02-28 02:46:00 +0900

    #10 EmailLoginActivity email 등록 버튼

--- app/src/main/java/com/example/mindgarden/ui/login/EmailSignUpActivity.kt
+++ app/src/main/java/com/example/mindgarden/ui/login/EmailSignUpActivity.kt
@@ -1,9 +1,14 @@
 package com.example.mindgarden.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.mindgarden.R
+import kotlinx.android.synthetic.main.activity_email_login.*
 import kotlinx.android.synthetic.main.activity_email_sign_up.*
+import kotlinx.android.synthetic.main.activity_email_sign_up.edt_email
 import kotlinx.android.synthetic.main.toolbar_write_diary.view.*
 
 class EmailSignUpActivity : AppCompatActivity() {
@@ -12,7 +17,6 @@ class EmailSignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email_sign_up)
 
-
         toolbar_email_sign_up.txt_date_toolbar_write_diary.text = "이메일 회원가입"
         edt_email.setOnFocusChangeListener { view, b ->
             if (android.util.Patterns.EMAIL_ADDRESS.matcher(edt_email.text).matches()) {
@@ -20,10 +24,22 @@ class EmailSignUpActivity : AppCompatActivity() {
             } else {
                 txt_check_email.setTextColor(getColor(R.color.colorPrimaryMint))
             }
-
-
         }
 
 
+        toolbar_email_sign_up.btn_save_diary_toolbar.setOnClickListener{
+            if(canEnroll()) {
+                val enrolledIntent = Intent(this, EmailLoginActivity::class.java)
+                startActivity(enrolledIntent)
+            }
+
+        }
+
     }
+
+    private fun canEnroll(): Boolean {
+        //유효성 판단하는 함수
+        return edt_email.toString().isNotEmpty()&&edt_name.toString().isNotEmpty()&&(edt_password.text.toString()==edt_password_check.text.toString())
+    }
+
 }
