25015e835fe1fc56ffa324f976af71d7387500c0
EuneeChung <chungee375@gmail.com>
2020-03-01 19:40:00 +0900

    #10 EmailSignUpActivity  실시간 이메일 유효성 검사 && 비밀번호 재확인 체크

--- app/src/main/java/com/example/mindgarden/ui/login/EmailSignUpActivity.kt
+++ app/src/main/java/com/example/mindgarden/ui/login/EmailSignUpActivity.kt
@@ -3,7 +3,11 @@ package com.example.mindgarden.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
 import com.example.mindgarden.Network.ApplicationController
 import com.example.mindgarden.Network.NetworkService
 import com.example.mindgarden.Network.POST.PostEmailSignUpResponse
@@ -23,18 +27,69 @@ class EmailSignUpActivity : AppCompatActivity() {
         ApplicationController.instance.networkService
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email_sign_up)
 
         toolbar_email_sign_up.txt_date_toolbar_write_diary.text = "이메일 회원가입"
         edt_email.setOnFocusChangeListener { view, b ->
-            if (android.util.Patterns.EMAIL_ADDRESS.matcher(edt_email.text).matches()) {
-                txt_check_email.setTextColor(getColor(R.color.colorWhite))
-            } else {
-                txt_check_email.setTextColor(getColor(R.color.colorPrimaryMint))
-            }
+
+            hasFocus(view, b) //포커스 잡혀있으면 green  border로 바꾸기
+
         }
+
+        //실시간 이메일인지  아닌지 확인
+        edt_email.addTextChangedListener (object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(edt_email.text).matches()) {
+                    txt_check_email.visibility = View.VISIBLE
+                    txt_check_email.setTextColor(getColor(R.color.colorRed))
+                }
+                else{
+                    txt_check_email.visibility=View.INVISIBLE
+                }
+            }
+        })
+
+        //이름
+        edt_name.setOnFocusChangeListener { view, b ->
+            hasFocus(view, b)
+        }
+        //비밀번호
+        edt_password.setOnFocusChangeListener { view, b ->
+            hasFocus(view, b)
+        }
+        //비밀번호 화인
+        edt_password_check.setOnFocusChangeListener { view, b ->
+            hasFocus(view, b)
+
+        }
+
+        edt_password_check.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (edt_password.text.toString() != edt_password_check.text.toString()) {
+                    txt_check_password_again.visibility = View.VISIBLE
+                    txt_check_password_again.setTextColor(getColor(R.color.colorRed))
+                }
+                else{
+                    txt_check_password_again.visibility=View.INVISIBLE
+                }
+            }
+        })
+
 
 
         toolbar_email_sign_up.btn_save_diary_toolbar.setOnClickListener {
@@ -45,13 +100,30 @@ class EmailSignUpActivity : AppCompatActivity() {
             }
 
         }
+        //뒤로가기 버튼 누를시 signinactivity로 이동
+        toolbar_email_sign_up.btn_back_toolbar.setOnClickListener {
+            finish()
+        }
 
+
+    }
+
+
+    //포커스 잡혔을 떄는 그린, 안잡혔을 때는 회색
+    fun hasFocus(view: View, b: Boolean) {
+        if (b) {
+            view.setBackgroundResource(R.drawable.grid_border)
+        } else {
+            view.setBackgroundResource(R.drawable.gray_border_square)
+        }
     }
 
     private fun canEnroll(): Boolean {
         //유효성 판단하는 함수
         return edt_email.toString().isNotEmpty() && edt_name.toString().isNotEmpty() && (edt_password.text.toString() == edt_password_check.text.toString())
     }
+
+
 
     fun postEmailSignUpResponse() {
         var jsonObject = JSONObject()
@@ -76,9 +148,8 @@ class EmailSignUpActivity : AppCompatActivity() {
                     if (response.body()!!.status == 200) {
                         if (response.body()!!.success) {
                             Log.e("회원가입 성공 메세지", response.body()!!.message)
-                        }
-                        else{
-                            Log.e("회원가입 메세지",response.body()!!.message)
+                        } else {
+                            Log.e("회원가입 메세지", response.body()!!.message)
                         }
 
                     }
