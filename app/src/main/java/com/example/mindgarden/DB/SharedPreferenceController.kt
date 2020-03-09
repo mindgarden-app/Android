b80bfb8045ee6d483d3eed1d0d948761c4717f08
EuneeChung <chungee375@gmail.com>
2020-03-09 18:44:27 +0900

    #25 SharedPreferenceController  ->user info editor 하나로 통합  ->Token clear 할 때 같이 클리어하기

--- app/src/main/java/com/example/mindgarden/DB/SharedPreferenceController.kt
+++ app/src/main/java/com/example/mindgarden/DB/SharedPreferenceController.kt
@@ -5,44 +5,51 @@ import android.content.SharedPreferences
 
 object SharedPreferenceController {
 
-    val PASSWORD="unique_string2"
-    val USER_NAME="unique_strng3"
-    val USER_MAIL="unique_strng4"
+
     val ALARMSTATE ="unique_strng5"
-    val PASSWORDSWITCHSTATE = "unique_strng5"
+    val PASSWORDSWITCHSTATE="passwordswitchstate"
+
+    val USER="user"
+
+    lateinit var  preference: SharedPreferences
+    lateinit var  editor:SharedPreferences.Editor
+
 
 
     //사용자 비밀번호
     fun setPassword(ctx: Context,pw:String){
-        val  preference: SharedPreferences=ctx.getSharedPreferences(PASSWORD,Context.MODE_PRIVATE)
-        val  editor:SharedPreferences.Editor=preference.edit()
+        preference=ctx.getSharedPreferences(USER,Context.MODE_PRIVATE)
+        editor=preference.edit()
         editor.putString("PW",pw)
         editor.apply()
     }
     fun getPassword(ctx:Context):String{
-        val preference:SharedPreferences=ctx.getSharedPreferences(PASSWORD,Context.MODE_PRIVATE)
+        preference=ctx.getSharedPreferences(USER,Context.MODE_PRIVATE)
+        editor=preference.edit()
         return preference.getString("PW","")
     }
     //사용자 이름
     fun  setUserName(ctx: Context,userName:String){
-        val  preference: SharedPreferences=ctx.getSharedPreferences(USER_NAME,Context.MODE_PRIVATE)
-        val  editor:SharedPreferences.Editor=preference.edit()
+        preference=ctx.getSharedPreferences(USER,Context.MODE_PRIVATE)
+        editor=preference.edit()
         editor.putString("u_name",userName)
         editor.commit()
     }
     fun getUserName(ctx:Context):String{
-        val preference:SharedPreferences=ctx.getSharedPreferences(USER_NAME,Context.MODE_PRIVATE)
+        preference=ctx.getSharedPreferences(USER,Context.MODE_PRIVATE)
+        editor=preference.edit()
         return preference.getString("u_name","")
     }
     // 사용자 메일
     fun setUserMail(ctx: Context,userMail:String){
-        val  preference: SharedPreferences=ctx.getSharedPreferences(USER_MAIL,Context.MODE_PRIVATE)
-        val  editor:SharedPreferences.Editor=preference.edit()
+        preference=ctx.getSharedPreferences(USER,Context.MODE_PRIVATE)
+        editor=preference.edit()
         editor.putString("u_mail",userMail)
         editor.commit()
     }
     fun getUserMail(ctx:Context):String{
-        val preference:SharedPreferences=ctx.getSharedPreferences(USER_MAIL,Context.MODE_PRIVATE)
+        preference=ctx.getSharedPreferences(USER,Context.MODE_PRIVATE)
+        editor=preference.edit()
         return preference.getString("u_mail","")
     }
 
