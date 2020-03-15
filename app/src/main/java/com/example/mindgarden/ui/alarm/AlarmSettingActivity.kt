a1487924a7f3ba48ce6a3057820574d338a490c8
Cho2018 <hjchojane@naver.com>
2020-03-15 15:01:12 +0900

    #33 Alarm - 서비스 삭제 / 알람 해제 완료

--- app/src/main/java/com/example/mindgarden/ui/alarm/AlarmSettingActivity.kt
+++ app/src/main/java/com/example/mindgarden/ui/alarm/AlarmSettingActivity.kt
@@ -13,12 +13,14 @@ import kotlinx.android.synthetic.main.toolbar_mypage_main.*
 import com.example.mindgarden.R
 import android.widget.TimePicker
 import android.app.AlarmManager
+import android.os.Build
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import com.example.mindgarden.db.SharedPreferenceController
 import android.view.WindowManager
-import com.example.mindgarden.service.AlarmService
+import com.example.mindgarden.broadcastReceiver.BroadcastD
 import com.example.mindgarden.ui.mypage.MypageActivity
 
 /*
@@ -57,18 +59,18 @@ class AlarmSettingActivity : AppCompatActivity() {
 
         alarmSwitch.setOnCheckedChangeListener { _, isChecked ->
             btnSetTimeState(isChecked)
-            if(!isChecked) alarmCancel()    //알람취소
+            if(!isChecked) alarmCancel()    //알람 취소
         }
     }
 
     //switch 상태 저장
     override fun onPause() {
         super.onPause()
-        SharedPreferenceController.setAlarmState(this ,alarmSwitch.isChecked)
+        SharedPreferenceController.setAlarmState(this, alarmSwitch.isChecked)
     }
 
-    //시간설정 버튼
-    fun btnSetTimeState(isChecked : Boolean){
+    //시간 설정 버튼
+    fun btnSetTimeState(isChecked : Boolean) {
         val btnSetTime: Button = findViewById(R.id.btnSetTime)   //시간 설정 버튼
         btnSetTime.setTextColor(Color.BLACK)
 
@@ -77,7 +79,6 @@ class AlarmSettingActivity : AppCompatActivity() {
             btnSetTime.setOnClickListener {
                 showDialog()
             }
-
         } else {
             //버튼 비활성화 + 글씨 색 바꾸기
             btnSetTime.isClickable = false
@@ -86,23 +87,23 @@ class AlarmSettingActivity : AppCompatActivity() {
     }
 
     //알림 시간을 설정하는 다이얼로그
-    fun showDialog(){
+    fun showDialog() {
         val builder = AlertDialog.Builder(this, R.style.AlarmDialogStyle)
         val dialogView = layoutInflater.inflate(R.layout.dialog_alarm_setting, null)
-        val alarmTimePicker= dialogView.findViewById<TimePicker>(R.id.alarmTimePicker)
+        val alarmTimePicker = dialogView.findViewById<TimePicker>(R.id.alarmTimePicker)
 
-        //시간설정
+        //시간 설정
         alarmTimePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
             alarmTimePicker(hourOfDay, minute)
         }
 
         builder.setView(dialogView)
 
-        builderNew= builder.show()
+        builderNew = builder.show()
         builderNew.window.setBackgroundDrawableResource(R.drawable.round_layout_border)
         builderNew.show()
 
-        //크기조절
+        //크기 조절
         val lp = WindowManager.LayoutParams()
         lp.copyFrom(builderNew.window.attributes)
 
@@ -110,7 +111,7 @@ class AlarmSettingActivity : AppCompatActivity() {
         window.attributes = lp
     }
 
-    fun mClick(v : View){
+    fun mClick(v : View) {
         when (v.id){
             R.id.btn_ok_alarm_setting -> {
                 setAlarm(triggerTime)
@@ -135,21 +136,38 @@ class AlarmSettingActivity : AppCompatActivity() {
         }
     }
 
+    //수정
     //알람설정
     fun setAlarm(tgTime: Long){
-        //수정
-        startService(Intent(this@AlarmSettingActivity, AlarmService::class.java).putExtra("alarmTime", tgTime).putExtra("index", 0))
-    }
+        //수정 //사용 안 함
+        //startService(Intent(this@AlarmSettingActivity, AlarmService::class.java).putExtra("alarmTime", tgTime).putExtra("index", 0))
 
-    //알람 해제
-    fun alarmCancel(){
-        //수정
-        //startService(Intent(this@AlarmSettingActivity, AlarmService::class.java).putExtra("index", 1))
+        setChannel()
 
-        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager     //AlarmManager
+        //알람이 발생했을 경우 BroadcastD에게 방송을 해주기 위해 명시
+        val intent = Intent(this, BroadcastD::class.java)
 
         val pendingIntent : PendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
 
+        //알람 예약
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager     //AlarmManager
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, tgTime, 24 * 60 * 60 * 1000, pendingIntent)
+        //alarmManager.set(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent)
+
+        Log.e("service", "alarmOk")
+    }
+
+    //수정
+    //알람 해제
+    fun alarmCancel(){
+        //수정 //사용 안 함
+        //startService(Intent(this@AlarmSettingActivity, AlarmService::class.java).putExtra("index", 1))
+
+        val intent = Intent(this, BroadcastD::class.java)
+
+        val pendingIntent : PendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager     //AlarmManager
         alarmManager.cancel(pendingIntent)
 
         val toast: Toast = Toast(this)
@@ -161,5 +179,23 @@ class AlarmSettingActivity : AppCompatActivity() {
         toastText.gravity = Gravity.CENTER
         toast.view = toastView
         toast.show()
+    }
+
+    //수정
+    fun setChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            //Register the channel with the system
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+            Log.e("service", "channelOk")
+        }
     }
 }
